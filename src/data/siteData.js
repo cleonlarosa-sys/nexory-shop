8c0d75822a94b618792063ce9fd233dccf6aad61
cleonlarosa-sys <cleonlarosa@gmail.com>
2026-06-15 14:06:32 -0500

    Update siteData.js

--- src/data/siteData.js
+++ src/data/siteData.js
@@ -65,7 +65,7 @@ export const PRODUCTS = [
     desc: "Carga rapida de 100w de potencia para tu celular, laptop o tablet. Pantalla Led con indicador de bateria + cable",
     price: "S/ 189",
     oldPrice: "S/ 219",
-    image: "/Power-Bank-UGREEN-20000mAh-100W-Carga-Super-Rapida-Portatil-PB720-3.png",
+    image: "https://i.imgur.com/xZyaM63.png",
     features: ["100W PD", "Pantalla Led"],
   },
   {
